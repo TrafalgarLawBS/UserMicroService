a9f5c0250b78a84d12726b9e7a8cbf48de2d58f9
it21-2014 <it21-2014@AR4-04>
2017-11-08 17:22:00 +0100

    cas 3

---
+++ UserMicroService.Tests/UserTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserMicroService.DataAccess;
+using UserMicroService.Models;
+
+namespace UserMicroService.Tests
+{
+    class UserTest
+    {
+        public void ClearUsers()
+        {
+            UserDB.listOfUsers.Clear();
+        }
+
+
+        [Test]
+        public void CreateUserSuccess()
+        {
+            User testUser = new User
+            {
+                Id = 1,
+                Name = "Marko"
+            };
+            UserDB.AddNewUser(testUser);
+            Assert.AreEqual(1, UserDB.listOfUsers.Count);
+        }
+    }
+}
---
+++ UserMicroService/DataAccess/UserDB.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UserMicroService.Models;
+
+namespace UserMicroService.DataAccess
+{
+    public static class UserDB
+    {
+        public static List<User> listOfUsers = new List<User>();
+
+
+        public static User GetUserById(int id) {
+            foreach (User u in listOfUsers) {
+                if (u.Id == id) {
+                    return u;
+                }
+            }
+            return null;
+        }
+
+        public static User GetUserByName(string name) {
+            foreach (User u in listOfUsers)
+            {
+                if (u.Name == name)
+                {
+                    return u;
+                }
+            }
+            return null;
+        }
+
+        public static List<User> GetAllUsers() {
+            return listOfUsers;
+        }
+
+        public static User AddNewUser(User user) {
+            listOfUsers.Add(user);
+            return GetUserById(user.Id);
+        }
+
+        public static void ModifyUser(User u) {
+
+            User user = GetUserById(u.Id);
+
+            listOfUsers.Remove(user);
+            listOfUsers.Add(u);            
+        }
+
+        public static void DeleteUser(int id) {
+            User user = GetUserById(id);
+            if (user != null)
+            {
+                listOfUsers.Remove(user);
+            }
+        }
+
+    }
+}
